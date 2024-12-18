b416a029a7b4093929f490f3bc9c20cd8e65e244
Uberoy <joacim.jansson@iths.se>
2024-12-18 17:16:06 +0100

    Added page for POST

---
+++ Dystopia.Frontend/Entities/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace Dystopia.Frontend.Entities;
+
+public class Ticket
+{
+    public string UserId { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+}
